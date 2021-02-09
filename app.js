052d1629ecac53ff60aad9134e116787a8bd865f
ntaylor6422 <ntaylor6422@gmail.com>
2021-02-09 11:11:31 +0900

    Added vue to project

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(express.static(path.resolve(__dirname, "..", "dist")));
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
 app.use("/api/", apiRoutes);
 app.use("/users", users);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "dist", "index.html"));
+});
 
 module.exports = app;
