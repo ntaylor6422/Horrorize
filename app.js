758d4bba9c7a107320cd0e44e7afca583e386f8d
ntaylor6422 <ntaylor6422@gmail.com>
2021-02-09 11:14:16 +0900

    Forgot path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const apiRoutes = require("./routes/allRoutes");
 const users = require("./routes/users");
 const app = express();
