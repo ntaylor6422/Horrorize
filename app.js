8dd64f6073d71e5f68314da550bf53aff59d1cc7
ntaylor6422 <ntaylor6422@gmail.com>
2021-02-09 11:57:53 +0900

    Changed dist path

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static(path.resolve(__dirname, "..", "dist")));
+app.use(express.static(path.resolve(__dirname, ".", "dist")));
 
 app.use("/api/", apiRoutes);
 app.use("/users", users);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, ".", "dist", "index.html"));
 });
 
 module.exports = app;
